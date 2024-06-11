96e88406884886a3238a787f8375a31659940b85
AGNESGITAU <agnesgitau40@gmail.com>
2024-06-11 21:09:56 +0300

    Create Queries, INTO JOIN

--- models/route.py
+++ models/route.py
@@ -4,3 +4,14 @@ class Route:
     def __init__(self, id=None, name=None):
         self.id = id
         self.name = name
+    
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        if self.id is None:
+            cursor.execute('INSERT INTO routes (name) VALUES (?)', (self.name,))
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute('UPDATE routes SET name = ? WHERE id = ?', (self.name, self.id))
+            conn.commit()
+            conn.close()
